5c18b0db91bc2ee44da5d1d1465880b6b9c58d15
Alexis Grimaldi <alexis.grimaldi+dev@gmail.com>
2011-06-04 18:40:48 +0200

    Successfully handles changing database.
    Changing the database is performed through modification
    of mongoose.connection.db.databaseName.

--- controllers/dataset.js
+++ controllers/dataset.js
@@ -5,32 +5,27 @@ var mongoose = require('mongoose')
 
 var route = function(app){
 
-  app.get('/datasets/:dataset', dbutils.connectdb, function(req, res){
+  app.get('/datasets/:dataset', dbutils.connect, function(req, res){
     res.render('dataset', {
       title: req.params.dataset
     , dataset: req.params.dataset 
     });
   });
 
-  app.get('/datasets/:dataset/:seqid', dbutils.connectdb, function(req, res){
+  app.get('/datasets/:dataset/:seqid', dbutils.connect, function(req, res){
     var reference = new Reference();
     reference.findById(req.params.seqid, function(err, doc){
       res.send(doc);
     });
   });
 
-  app.get('/datasets/:dataset/:seqid/:start/:stop/:tracks', dbutils.connectdb, function(req, res){
+  app.get('/datasets/:dataset/:seqid/:start/:stop/:tracks', dbutils.connect, function(req, res){
     var tracksNames = req.params.tracks
       , track = Track(tracksNames, tracksNames);
     track.find({
       seqid: req.params.seqid
     }, function(err, docs){
-      try {
-      console.log(docs);
       res.send(docs);
-      } catch (e) {
-        console.log(e);
-      }
     });
   });
 
