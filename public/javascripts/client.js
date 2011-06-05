1989f5365d1b8cec57ae995143bb1a589dc9855f
Alexis Grimaldi <alexis.grimaldi+dev@gmail.com>
2011-06-05 20:05:20 +0200

    Fixed bug in dataset selection.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
 
   /**
-   * Function to handle a selected dataset in a dropdown menu
-   *
-   * @param {Object} dropdown menu
-   * @return {Boolean}
-   * @api public
-   */
-  function OnSelect(dropdown)
-  {
-    var myindex  = dropdown.selectedIndex
-    var SelValue = dropdown.options[myindex].value
-    if (SelValue != 'Select a dataset') {
-      var baseURL  = '/browse/'+SelValue
-      top.location.href = baseURL;
-      return true;
-    }
-    return false;
-  }
-
-  /**
-   * Handle browse form.
+   * Handle browsing from main form.
    */
   $("#submit").click(function() {
     var seqid = $('#seqid').val()
@@ -33,12 +14,28 @@ $(document).ready(function() {
     }, function(data) {
       $("#tracks").empty();
       $("#tracks").append(JSON.stringify(data));
-      //for (var i in data){
-        //console.log(data[i]);
-        //$("#tracks").append(data[i]);
-      //}
     });
     return false;
   });
 
 });
+
+
+/**
+ * Function to handle a selected dataset in a dropdown menu
+ *
+ * @param {Object} dropdown menu
+ * @return {Boolean}
+ * @api public
+ */
+function OnSelect(dropdown)
+{
+  var myindex  = dropdown.selectedIndex
+  var SelValue = dropdown.options[myindex].value
+  if (SelValue != 'Select a dataset') {
+    var baseURL  = '/browse/'+SelValue
+    top.location.href = baseURL;
+    return true;
+  }
+  return false;
+}
