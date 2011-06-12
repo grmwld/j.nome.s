1f4f6066aeaf51f3db41aef7cab0fc65e471b227
Alexis GRIMALDI <alexis.grimaldi+dev@gmail.com>
2011-06-12 14:16:32 +0200

    Removed debug messages.

--- public/javascripts/rulersUI.js
+++ public/javascripts/rulersUI.js
@@ -167,8 +167,6 @@ Raphael.fn.drawDocument = function(doc, view_start, view_end, style) {
   var rel_start = (((Math.max(doc.start, view_start) - view_start) / view_span) * (this.width-100)) + 50;
   var rel_end = (((Math.min(doc.end, view_end) - view_start) / view_span) * (this.width-100)) + 50;
   var rel_doc_length = rel_end - rel_start;
-  console.log(rel_start);
-  console.log(rel_doc_length);
   var d = this.rect(rel_start, this.height/3, rel_doc_length, this.height/3).attr(style);
   d.attr({title:[
     'ID : ' + doc._id,
