3b108fe488396bdc38896b3b93df36871329a8ac
Alexis GRIMALDI <alexis.grimaldi+dev@gmail.com>
2011-06-18 18:17:17 +0200

    Moved navigation-specific raphael exptensions to navigation.js

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -287,3 +287,135 @@ RatioZoom.prototype.refresh = function(cur_span, style){
   self.clear();
   self.draw(cur_span, style);
 }
+
+
+
+
+/**
+ * Draw the labeled ruler
+ *
+ * @param {Number} view_start
+ * @param {Number} view_end
+ * @param {Object} style
+ */
+Raphael.fn.drawMainRuler = function(view_start, view_end, style) {
+  var nf = new PHP_JS().number_format
+    , view_span = view_end - view_start
+    , view_step = view_span / 5
+    , ruler = this.set();
+  ruler.push(this.lineTo(50, 2*this.height/3, this.width-50, 2*this.height/3).attr(style));
+  for (var x = 50.5; x <= this.width-50; x += 40) {
+    ruler.push(this.lineTo(x, 2*this.height/3-3, x, 2*this.height/3+3).attr(style));
+  }
+  for (var x = 50; x <= this.width-50; x += 200) {
+    ruler.push(this.text(x, 12, nf(view_start+view_step*((x-50)/200))));
+    ruler.push(this.lineTo(x+0.5, 2*this.height/3-8, x+0.5, 2*this.height/3+8).attr(style));
+  }
+  return ruler;
+}
+
+/**
+ * Display 2 lines going from the current selection in the overview to the local view
+ *
+ * @param {Number} cur_span
+ * @param {Object} style
+ */
+Raphael.fn.drawRatio = function(cur_span, style) {
+  var sx1 = cur_span.attrs.x
+    , sy1 = 0
+    , sx2 = 50
+    , sy2 = this.height
+    , ex1 = cur_span.attrs.x + cur_span.attrs.width
+    , ey1 = 0
+    , ex2 = this.width - 50
+    , ey2 = this.height,
+    ratio = this.set();
+  ratio.push(this.lineTo(sx1, sy1, sx2, sy2).attr(style));
+  ratio.push(this.lineTo(ex1, ey1, ex2, ey2).attr(style));
+  return ratio;
+}
+
+/**
+ * Display the area currently selected
+ *
+ * @param {Number} view_start
+ * @param {Number} view_end
+ * @param {Number} tot_length
+ * @param {Object} style
+ * @return {SVG}
+ */
+Raphael.fn.currentSpan = function(view_start, view_end, tot_length, style) {
+  var view_span = view_end - view_start
+    , rel_start = (((view_start) / tot_length) * (this.width-100)) + 50
+    , rel_end = (((view_end) / tot_length) * (this.width-100)) + 50
+    , rel_doc_length = rel_end - rel_start
+  return this.rect(rel_start, 0, rel_doc_length, this.height, 5).attr(style);
+}
+
+/**
+ * Add a selectable area to a Paper.
+ *
+ * @param {Number} view_start
+ * @param {Number} view_end
+ * @param {Object} style
+ */
+Raphael.fn.explorableArea = function(view_start, view_end, style, callback) {
+  var gs, ge
+    , view_span = view_end - view_start
+    , a = this.rect(0, 0, this.width, this.height).attr({
+        'fill-opacity': 0,
+        'stroke-opacity': 0,
+        fill: "#eee"
+      });
+  a.drag(
+    // Mouse move
+    function(dx, dy, x, y, e) {
+      if (dx > 0) { 
+        this.selector.attr({
+          x: this.selector.ox
+        , width: dx-1
+        });
+      } else {
+        this.selector.attr({
+          x: e.offsetX+1
+        , width: -dx
+        });
+      }
+    },
+    // Mouse down
+    function(x, y, e) {
+      if (!e.offsetX) {
+        e.offsetX = e.clientX - $(e.target).position().left;
+      }
+      gs = Math.floor((((e.offsetX-50)/(this.paper.width-100)) * view_span) + view_start);
+      this.selector = this.paper.rect(e.offsetX, 0, 1, this.attr("height"), 5).attr(style); 
+      this.selector.ox = this.selector.attr("x");
+    },
+    // Mouse up
+    function(e) {
+      var goto_start
+        , goto_end;
+      if (!e.offsetX) {
+        e.offsetX = e.clientX - $(e.target).position().left;
+      }
+      ge = Math.floor((((e.offsetX-50)/(this.paper.width-100)) * view_span) + view_start);
+      goto_start = Math.min(gs, ge);
+      goto_end = Math.max(gs, ge);
+      this.selector.remove();
+      // Span selection
+      if (goto_start != goto_end) {
+        sanitizeInputPos(goto_start, goto_end, function(start, end){
+          callback(start, end);
+        });
+      }
+      // Location click
+      else {
+        var i_span = Math.floor((parseNum($('#end').val()) - parseNum($('#start').val())) / 2);
+        sanitizeInputPos(goto_start-i_span, goto_end+i_span, function(start, end){
+          callback(start, end);
+        });
+      }
+    }
+  );
+  return a;
+}
