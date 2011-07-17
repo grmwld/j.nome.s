4129b51d6329e819e0ebf66cc73fd8700d505c2c
Alexis GRIMALDI <alexis.grimaldi+dev@gmail.com>
2011-07-17 10:06:43 +0200

    Fixed bug in rendering profiles for views < 2kb

--- src/cutils.cpp
+++ src/cutils.cpp
@@ -40,7 +40,7 @@ static Handle<Value> processProfile(const Arguments& args)
     start = input->Get(zero)->ToObject()->Get(kstart)->NumberValue();
     view_start = start;
     view_end = input->Get(Number::New(input_length-1))->ToObject()->Get(kend)->NumberValue();
-    step = std::min((view_end - view_start) / 2000, (unsigned int)1000);
+    step = std::min(((view_end - view_start) / 2000) + 1, (unsigned int)1000);
     output = Array::New(2000);
 
     for (i = 0; i < input_length; ++i)
