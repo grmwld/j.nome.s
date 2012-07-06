08f4b2a2e0264780075f5ac19c967fac4b4fc0e6
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2012-07-06 21:33:49 +0200

    Add test for non-existing bigwig

--- test/test-models.js
+++ test/test-models.js
@@ -234,6 +234,40 @@ describe('Track', function() {
 
     });
 
+    describe('with non-existent bigwig profile metadata', function() {
+      var track = new Track(dataset, {
+        id: 'rnaseq-bigwig'
+      , name: 'RNASeq data bigwig'
+      , description: 'RNA-Seq data from SRR002051 (bigwig)'
+      , type: 'profile'
+      , backend: 'bigwig'
+      , file: './test/store/non-existent.profile.bw'
+      , style: {
+          gutter: 25
+        , shade: true
+        , nostroke: true
+        , axis: '0 0 1 1'
+        , axisxstep: 10
+        , axisystep: 4
+        }
+      });
+
+      it('responds with an empty array', function(done) {
+        expect(track).to.be.an.instanceof(TrackProfile);
+        track.fetchInInterval('chrIV', null, 32289, 3933090, function(err, docs) {
+          try {
+            expect(err).to.not.exist;
+            expect(docs).to.be.an.instanceof(Array);
+            expect(docs).to.have.length(0);
+            done(err);
+          } catch(err) {
+            done(err);
+          }
+        });
+      });
+
+    });
+
     describe('with oriented profile metadata', function() {
       var track = new Track(dataset, {
         id: 'rnaseq_oriented'
