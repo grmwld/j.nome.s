05397e4ea512c58f1da97213c4013a7bdb10cd59
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2012-07-15 12:28:46 +0200

    Separate drawing code

--- public/javascripts/app/glyph-base.js
+++ public/javascripts/app/glyph-base.js
@@ -68,6 +68,44 @@ GlyphBase.prototype.adjustToLayer = function(layer) {
 };
 
 /**
+ * Associate a tooltip to a glyph
+ */
+GlyphBase.prototype.tooltip = function() {
+  var tooltip_text = [];
+  for (var i in this.document) {
+    tooltip_text.push('<strong>'+i+'</strong>' + ' : ' + this.document[i]);
+  }
+  this.glyph.forEach(function(e) {
+    $(e.node).qtip({
+      content: { text: tooltip_text.join('<br />') },
+      style: { classes: 'ui-tooltip-bootstrap' }
+    });
+    e.attr({ cursor: 'help' });
+  });
+}
+
+/**
+ * Draw the label associated to a document
+ *
+ * @param {Object} shape
+ * @param {String} txt
+ * @param {Boolean} packed
+ */
+GlyphBase.prototype.label = function(shape, txt, packed) {
+  var shape_bbox = shape.getBBox();
+  var text = this.canvas.text(shape_bbox.x, shape_bbox.y-1-shape_bbox.height/2).attr({
+    'text': txt,
+    'text-anchor': 'start'
+  });
+  var text_bbox = text.getBBox();
+  if (typeof txt === 'undefined' || (packed && text_bbox.width > shape_bbox.width)) {
+    text.remove();
+    text = null;
+  }
+  return text
+}
+
+/**
  * Draw a feature element
  *
  * @param {Object} style
@@ -77,44 +115,21 @@ GlyphBase.prototype.adjustToLayer = function(layer) {
 GlyphBase.prototype.draw = function(style, packed) {
   self = this;
   var view_span = this.viewEnd - this.viewStart;
-  var nf = new PHP_JS().number_format;
   var rel_start = (((Math.max(this.document.start, this.viewStart) - this.viewStart) / view_span) * (this.canvas.width-100)) + 50;
   var rel_end = (((Math.min(this.document.end, this.viewEnd) - this.viewStart) / view_span) * (this.canvas.width-100)) + 50;
   var rel_doc_length = rel_end - rel_start;
-  var title = [];
-  var doc_text = null;
   var doc_element = this.canvas.set();
-  var doc_shape_bbox = null;
-  var doc_text_bbox = null;
   var packed = typeof packed !== 'undefined' ? packed : true;
   var doc_shape = this.drawShape(rel_start, rel_end);
-  for (var i in self.document) {
-    title.push('<strong>'+i+'</strong>' + ' : ' + self.document[i]);
-  }
+  var doc_text = this.label(doc_shape, this.document.name, packed);
   doc_shape.attr(style);
-  doc_shape_bbox = doc_shape.getBBox();
-  doc_text = this.canvas.text(doc_shape_bbox.x, doc_shape_bbox.y-1-doc_shape_bbox.height/2).attr({
-    'text': self.document.name,
-    'text-anchor': 'start'
-  });
-  doc_text_bbox = doc_text.getBBox();
   doc_element.push(doc_shape);
-  if (self.document.name === undefined || (packed && doc_text_bbox.width > doc_shape_bbox.width)) {
-    doc_text.remove();
-  } else {
+  if (doc_text) {
     doc_element.push(doc_text);
   }
-  doc_element.attr({
-    cursor: 'help'
-  });
-  doc_element.forEach(function(e) {
-    $(e.node).qtip({
-      content: { text: title.join('<br />') },
-      style: { classes: 'ui-tooltip-bootstrap' }
-    });
-  });
   this.bbox = doc_element.getBBox();
   this.glyph = doc_element;
+  this.tooltip();
   return doc_element;
 };
 
